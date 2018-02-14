67a35e5cc0652acaef1aa0459beb2942f4403501
Karina <zhigimont.k@gmail.com>
2018-02-14 19:16:47 +0300

    Fix task 4

--- task 4/src/by/epam/preTraining/task4/model/Fibonacci.java
+++ task 4/src/by/epam/preTraining/task4/model/Fibonacci.java
@@ -1,16 +1,13 @@
 package by.epam.preTraining.task4.model;
 
-/**
- * Created by karina on 13-02-2018.
- */
 public class Fibonacci {
     public static int calculateFibonacciRecursive(int number) {
         int result;
-        if (number > 2){
+        if (number > 2) {
             result = calculateFibonacciRecursive(number - 1) + calculateFibonacciRecursive(number - 2);
-        } else if (number == 2){
+        } else if (number == 2) {
             result = 1;
-        } else if (number == 1){
+        } else if (number == 1) {
             result = 0;
         } else {
             result = -1;
@@ -20,11 +17,7 @@ public class Fibonacci {
 
     public static int calculateFibonacci(int number) {
         int result = 1;
-        if (number <= 0){
-            result = -1;
-        } else if (number == 1){
-            result = 0;
-        } else {
+        if (number > 1) {
             int fibo1 = 0;
             int fibo2 = 1;
             for (int i = 2; i < number; i++) {
@@ -32,6 +25,10 @@ public class Fibonacci {
                 fibo1 = fibo2;
                 fibo2 = result;
             }
+        } else if (number == 1) {
+            result = 0;
+        } else {
+            result = -1;
         }
         return result;
     }
