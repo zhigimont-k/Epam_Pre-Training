c2ff87f3699f8af41a7e4fe0c0b3da8ca8936389
Karina <zhigimont.k@gmail.com>
2018-02-15 21:50:31 +0300

    Fix task 4

--- task 4/src/by/epam/preTraining/task4/model/HanoiTower.java
+++ task 4/src/by/epam/preTraining/task4/model/HanoiTower.java
@@ -4,7 +4,7 @@ public class HanoiTower {
     private static StringBuilder resultRecursive = new StringBuilder();
 
     public static StringBuilder calculateHanoiRecursive(int rings, String from, String to, String middle) {
-        if (rings > 0){
+        if (rings > 0) {
             calculateHanoiRecursive(rings - 1, from, middle, to);
             resultRecursive.append("\n" + from + "->" + to);
             calculateHanoiRecursive(rings - 1, middle, to, from);
@@ -15,17 +15,15 @@ public class HanoiTower {
     public static StringBuilder calculateHanoi(int rings, String from, String to, String middle) {
         StringBuilder result = new StringBuilder();
         int bound = 1 << rings;
-        if (rings % 2 == 0) {
-            for (int i = 1; i < bound; i++) {
-                result.append("\n" + computeTowerName((i & i - 1) % 3, from, to, middle) + "->" +
+        boolean isEven = rings % 2 == 0;
+        if (!isEven){
+            String buf = middle;
+            middle = to;
+            to = buf;
+        }
+        for (int i = 1; i < bound; i++) {
+            result.append("\n" + computeTowerName((i & i - 1) % 3, from, to, middle) + "->" +
                         computeTowerName(((i | i - 1) + 1) % 3, from, to, middle));
-            }
-        } else {
-            for (int i = 1; i < bound; i++) {
-                result.append("\n" + computeTowerName((i & i - 1) % 3, from, middle, to) + "->" +
-                        computeTowerName(((i | i - 1) + 1) % 3, from, middle, to));
-            }
-
         }
 
         return result;
